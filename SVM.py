bc387ff3c319caa6c7fc565ef48e51a488b8eab8
Diasisme <diasisme@gmail.com>
2021-02-02 02:32:46 +0700

    Add encode dataframe for machine learning

--- SVM.py
+++ SVM.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 from sklearn import svm
 import database as db
 
 
 class Prediction:
-    # initiation class Databaase from database.py
-    archive = db.Database()
 
     def prediction_SVM(self):
         # make dataframe from database climate.db
-        self.df = pd.read_sql_query('select * from rain', self.archive.conn)
+        self.df = pd.read_csv('D:\Prediction_Thesis\csv\Climate.csv')
 
         #Transforming data
         self.df = self.df.replace(', ' , '-', regex= True)
-        print(self.df.head())
-        print(self.df.dtypes)
+        self.df['planting_time'] = LabelEncoder.fit_transform(self.df['planting_time'])
+        self.df_enc = pd.get_dummies(self.df, columns=["region"])
+        pd.set_option('display.max_columns', None)
+        print(self.df_enc.head())
+        print(self.df_enc.dtypes)
 
 
 
